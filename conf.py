52ba3b341dc42f03e293030dca896aa40865c8de
mattscicluna <mattcscicluna@gmail.com>
2017-03-24 10:16:24 -0400

    added rate limit checks

---
+++ conf.py
@@ -0,0 +1,8 @@
+#  Below are arguments you can change to better suit your purposes!
+
+#  Message to be tweeted prior to doing friendly action!
+HELLO_MESSAGE = "Hello World! Time to begin my daily routine of Liking and Favouriting Tweets from all you good people!"
+
+#  Message to be sent at beginning of run
+GOOD_MORNING_MESSAGE = "Good Morning World and all who inhabit it!"
+
--- main.py
+++ main.py
@@ -1,8 +1,44 @@
 import tweepy, time
 import permissions  # Get permissions dictionary
+import conf  # customize text in tweets and DMs sent by program
+
+
+def being_nice(api, old_followers_list, new_followers_list):
+    api.update_status(conf.HELLO_MESSAGE)
+    recent_follows = set(new_followers_list).intersection(old_followers_list)
+
+    #  how many favorites are left this cycle?
+    favs_left = api.rate_limit_status()['resources']['favorites']['/favorites/list']['remaining']
+
+    #  distrubute whats left among the new follows:
+    limit_favs = max(1, favs_left // len(recent_follows))
+
+    for follower_id in recent_follows:
+        list_of_tweets = api.user_timeline(follower_id)  # collect tweets
+        for tweet in list_of_tweets[:limit_favs]:
+            try:
+                api.create_favorite(tweet.id)  # favorite the tweet
+                time.sleep(2)
+                print("successfully favorited tweet {} from user {}".format(tweet.id, follower_id))
+            except tweepy.TweepError:
+                print("already favorited tweet {} from user {}".format(tweet.id, follower_id))
+            try:
+                api.retweet(tweet.id)  # re-tweet the tweet
+                time.sleep(2)
+                print("successfully re-tweeted tweet {} from user {}".format(tweet.id, follower_id))
+            except tweepy.TweepError:
+                print("already re-tweeted tweet {} from user {}".format(tweet.id, follower_id))
+
+
+def find_new_followers(api):
+    print("test")
 
 
 def run(consumer_key, consumer_secret, access_token, access_token_secret):
+
+    # intialize followers list
+    old_followers_list = []
+    new_followers_list = []
 
     # verify credentials
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
@@ -11,22 +47,26 @@ def run(consumer_key, consumer_secret, access_token, access_token_secret):
     # log into twitter api
     api = tweepy.API(auth)
 
-    # collect followers
-    follower_list = api.followers_ids()
+    api.update_status(conf.GOOD_MORNING_MESSAGE)
 
-    for follower_id in follower_list:
-        list_of_tweets = api.user_timeline(follower_id)  # collect tweets
-        for tweet in list_of_tweets:
-            try:
-                api.create_favorite(tweet.id)  # favorite the tweet
-                time.sleep(2)
-            except tweepy.TweepError:
-                print("already favorited! Moving on...")
-            try:
-                api.retweet(tweet.id)  # re-tweet the tweet
-                time.sleep(2)
-            except tweepy.TweepError:
-                print("already re-tweeted! Moving on...")
+    while True:
+
+        #  collect followers
+        find_new_followers(api)
+
+        #  Be nice to new followers!
+        new_followers_list = api.followers_ids()
+        being_nice(api, old_followers_list, new_followers_list)
+        old_followers_list = new_followers_list[:]
+
+        #  Wait until next round before next round!
+        time_until_next = api.rate_limit_status()['resources']['favorites']['/favorites/list']['reset']
+        print("Stopping cycle at time: {} to comply with Twitter rate limits"
+              ". Will resume at: {}".format(time.localtime(), time_until_next))
+        while time.localtime(time_until_next) > time.localtime():
+            time.sleep(10)
+
+        print("Resuming cycle! Time: {}".format(time.localtime()))
 
 
 if __name__ == "__main__":
