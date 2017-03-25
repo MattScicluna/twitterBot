264381ab01071e75b4bc4ab550d78f8fc3896e15
Matthew Scicluna <mattcscicluna@gmail.com>
2017-03-25 07:35:47 -0400

    Create permissions.py

---
+++ permissions.py
@@ -0,0 +1,6 @@
+#Insert Authentication details here as described in Blog Post.
+
+PERM_DICT = {'CONSUMER_KEY': '',
+             'CONSUMER_SECRET': '',
+             'ACCESS_TOKEN': '',
+             'ACCESS_TOKEN_SECRET': ''}
