94dbe40aa8c94a57b8300800c7fd9169eb86d093
mattscicluna <mattcscicluna@gmail.com>
2017-03-26 11:01:37 -0400

    working now?

--- permissions.py
+++
@@ -1,7 +0,0 @@
-#Insert Authentication details here as described in 
-#https://mattscicluna.github.io/Twitter%20Bots%20Really%20Affecting%20the%20US%20Election.html
-
-PERM_DICT = {'CONSUMER_KEY': '',
-             'CONSUMER_SECRET': '',
-             'ACCESS_TOKEN': '',
-             'ACCESS_TOKEN_SECRET': ''}
